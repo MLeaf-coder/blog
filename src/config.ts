1db502c9142db1f6dc28d68dcc49d4bab0dc0dc7
Sun Huaping <106392719+MLeaf-coder@users.noreply.github.com>
2024-12-15 18:37:30 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fengye',
+  title: 'FY',
   url: 'https://fengyegf.cn',
   subtitle: 'Demo Site',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
